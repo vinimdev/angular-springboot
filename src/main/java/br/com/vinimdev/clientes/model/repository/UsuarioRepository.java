8ce892faec60ed70d7cd918e3f42902699edf9b0
Vinicius Meira <viniciusmeirabr@hotmail.com>
2022-06-25 18:17:56 -0300

    Configurando o Spring Security

--- src/main/java/br/com/vinimdev/clientes/model/repository/UsuarioRepository.java
+++ src/main/java/br/com/vinimdev/clientes/model/repository/UsuarioRepository.java
@@ -3,5 +3,10 @@ package br.com.vinimdev.clientes.model.repository;
 import br.com.vinimdev.clientes.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByUsername(String username);
+
 }
